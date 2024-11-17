56953dc728525a187c6a19c3e8261a6c22201056
Jeffrey <kingtingthegreat@gmail.com>
2024-11-17 10:21:09 -0500

    slight refactor. only show confetti when certain score threshold is met

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ import {
   clearHighlights,
 } from "./utils/highlight.js";
 import { createScoreGradient, displayScoreMessage } from "./utils/score.js";
-import { showTooltip, hideTooltip } from "./utils/tooltip.js";
 import { SCANNER, WEBSITE } from "./domain.js";
 
 let selection = "";
@@ -65,47 +64,6 @@ function updateButtonState(selectedButtonId) {
   // Add 'selected' class to the clicked button
   document.getElementById(selectedButtonId).classList.add("selected");
 }
-
-// Tooltip content for each info button
-const tooltips = {
-  "info-contrasting-colors":
-    "This button checks if the text on the page has sufficient color contrast for readability. It checks for at least a 4.5:1 ratio of rbg values for the background color and text color.",
-  "info-large-text":
-    "This button checks if the text size is large enough (at least size 16) for visually impaired users.",
-  "info-labeled-images":
-    "This button checks if images have labels, which improves accessibility for screen reader users.",
-};
-
-// Add event listeners to info icons
-document
-  .getElementById("info-contrasting-colors")
-  .addEventListener("mouseenter", (event) => {
-    showTooltip(event, tooltips["info-contrasting-colors"]);
-  });
-
-document
-  .getElementById("info-contrasting-colors")
-  .addEventListener("mouseleave", hideTooltip);
-
-document
-  .getElementById("info-large-text")
-  .addEventListener("mouseenter", (event) => {
-    showTooltip(event, tooltips["info-large-text"]);
-  });
-
-document
-  .getElementById("info-large-text")
-  .addEventListener("mouseleave", hideTooltip);
-
-document
-  .getElementById("info-labeled-images")
-  .addEventListener("mouseenter", (event) => {
-    showTooltip(event, tooltips["info-labeled-images"]);
-  });
-
-document
-  .getElementById("info-labeled-images")
-  .addEventListener("mouseleave", hideTooltip);
 
 //clear button
 document.getElementById("clear-button").addEventListener("click", function () {
@@ -186,7 +144,7 @@ function performScan(scanType) {
                 "visible";
               document.getElementById("score").innerHTML = data.score;
               createScoreGradient(data.score);
-              displayScoreMessage(selection, data.score);
+              displayScoreMessage(selection, data);
 
               // Show score bar
               document.getElementById("score-bar").style.visibility = "visible";
@@ -204,7 +162,6 @@ function performScan(scanType) {
                 scanType == "Contrasting Colors" ||
                 scanType == "Large Text"
               ) {
-                console.log("test console log");
                 chrome.scripting.executeScript({
                   target: { tabId: activeTab.id },
                   function: highlightInaccessibleElements,
--- utils/score.js
+++ utils/score.js
@@ -1,3 +1,5 @@
+import { playConfetti } from "./play-confetti.js";
+
 //score range
 export function createScoreGradient(score) {
   const gradientContainer = document.getElementById("score-bar");
@@ -15,32 +17,37 @@ export function createScoreGradient(score) {
 }
 
 // Score feedback message
-export function displayScoreMessage(scanType, score) {
+export function displayScoreMessage(scanType, data) {
   const scoreMessageElement = document.getElementById("score-message");
 
   // Determine the message based on scan type and score
   let message = "";
 
   if (scanType === "Contrasting Colors") {
-    if (score < 100) {
+    if (data.score < 90) {
       message =
         "The highlighted elements don't satisfy the 4.5:1 rgb ratio for text color and its background color. Adjust the rgb values in the highlighted elements to improve your score.";
     } else {
       message = "Great job!";
+      playConfetti();
     }
   } else if (scanType === "Large Text") {
-    if (score < 100) {
+    if (data.score < 90) {
       message =
         "The highlighted elements are less than 16 point font. Increase the size font for these elements to improve your score.";
     } else {
       message = "Great job!";
+      playConfetti();
+    }
+  } else if (scanType === "Labeled Images") {
+    if (data.images_with_alt !== data.total_images) {
+      message =
+        "Some of images are missing alt text. Add alt text to these image tags.";
+    } else {
+      message = "Great job!";
+      playConfetti();
     }
   }
-  confetti({
-    particleCount: 100, // Number of confetti particles
-    spread: 70, // Spread of the confetti
-    origin: { x: 0.5, y: 0.5 }, // Origin of the confetti (center of the screen)
-  });
 
   // Set the message in the DOM
   scoreMessageElement.textContent = message;
