86bc82146c8612eba33ad1bceaf1ef2889dc0ad6
Jeffrey <kingtingthegreat@gmail.com>
2024-12-03 17:20:22 -0500

    create signout button

--- secret.js
+++ secret.js
@@ -1,20 +1,35 @@
 const SECRET_KEY = "SECRET";
 
+const signedInElement = document.getElementById("signed-in");
+const signOutButton = document.getElementById("sign-out-button");
+
 export function getSecret() {
   return localStorage.getItem(SECRET_KEY);
 }
 
 export function setSecret(secret) {
   localStorage.setItem(SECRET_KEY, secret);
+  if (secret) {
+    signedInElement.innerText = "Signed In";
+    signOutButton.style.visibility = "visible";
+  } else {
+    signedInElement.innerText = "Not Signed In";
+    signOutButton.style.visibility = "hidden";
+  }
 }
 
 // load previously saved value on page load
 document.addEventListener("DOMContentLoaded", () => {
-  const signedInElement = document.getElementById("signed-in");
   const savedValue = getSecret();
   if (savedValue) {
     signedInElement.innerText = "Signed In";
+    signOutButton.style.visibility = "visible";
   } else {
     signedInElement.innerText = "Not Signed In";
+    signOutButton.style.visibility = "hidden";
   }
+});
+
+signOutButton.addEventListener("click", () => {
+  setSecret("");
 });
